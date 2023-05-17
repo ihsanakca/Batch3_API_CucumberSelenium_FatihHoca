9b2abc5e0d6615f6cfb9a88b1c32445422da9008
HP <ihsanakcair@gmail.com>
2023-05-17 15:53:26 +0300

    Register feature finished

--- src/test/java/com/exlab/request/ExlabRequest.java
+++ src/test/java/com/exlab/request/ExlabRequest.java
@@ -2,25 +2,26 @@ package com.exlab.request;
 
 import com.exlab.common.DataForApi;
 import com.exlab.utilities.ConfigurationReader;
-
-import static io.restassured.RestAssured.*;
-
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
-public class ExlabRequest {
-    static Response response;
-    static int userID;
+import static io.restassured.RestAssured.*;
 
-    private static Response registerNewUser(String name, String email, String password, String about, String terms) {
+public class ExlabRequest {
+    public static Response response;
+    public static int userID;
+    public static String token;
+
+    public static Response registerNewUser(String name, String email, String password, String about, String terms) {
         response = given().accept(ContentType.JSON)
                 .and()
                 .body(DataForApi.registerUserBody(name, email, password, about, terms))
                 .when()
                 .post(ConfigurationReader.get("postRegisterUser"));
 
-         userID = response.path("id");
+        userID = response.path("id");
         System.out.println("userID = " + userID);
+        token=response.path("token");
 
         return response;
     }
